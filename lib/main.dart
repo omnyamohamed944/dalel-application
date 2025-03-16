8b06e1f0a119a735862873d2568664b50c4c3a86
Omnya Test <omneyamohamed944@gmail.com>
2025-03-16 15:37:28 +0200

    Updated project files

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const dalel());
+}
+
+class dalel extends StatelessWidget {
+  const dalel({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      
+    );
+  }
+}
